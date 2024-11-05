8450f8d22ae2fff52ebdc9dcbc1381cc15d78c02
alex-waiganjo <alexwaiganjo934@gmail.com>
2024-11-05 16:21:07 +0300

    get columns route

--- FileUpload/files/views.py
+++ FileUpload/files/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import File
 from .serializers import FileSerializer
-from .helpers import parse_excel,parse_pdf
+from .helpers import parse_excel, parse_pdf
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework import status
 import pandas as pd
@@ -46,7 +46,7 @@ class FileUploadView(APIView):
 
 
 class FileListView(generics.ListAPIView):
-     # Throttle Scope
+    # Throttle Scope
     throttle_scope = "high"
 
     queryset = File.objects.all()
@@ -54,7 +54,7 @@ class FileListView(generics.ListAPIView):
 
 
 class FileDetailView(generics.RetrieveUpdateDestroyAPIView):
-     # Throttle Scope
+    # Throttle Scope
     throttle_scope = "low"
 
     queryset = File.objects.all()
@@ -62,14 +62,14 @@ class FileDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class FilePreviewView(APIView):
-     # Throttle Scope
+    # Throttle Scope
     throttle_scope = "high"
 
     def get(self, request, pk):
         try:
             # Retrieve file instance by id
             file_instance = File.objects.get(pk=pk)
-            
+
             # Check file type and parse accordingly
             if file_instance.file_type == 'excel':
                 parsed_data = parse_excel(file_instance.file)
@@ -77,10 +77,10 @@ class FilePreviewView(APIView):
                 parsed_data = parse_pdf(file_instance.file)
             else:
                 return Response({"error": "Unsupported file type."}, status=status.HTTP_400_BAD_REQUEST)
-            
+
             # Return parsed data in JSON format
             return Response(parsed_data, status=status.HTTP_200_OK)
-        
+
         except File.DoesNotExist:
             return Response({"error": "File not found."}, status=status.HTTP_404_NOT_FOUND)
 
@@ -89,15 +89,12 @@ class ColumnDataView(APIView):
     # Throttle Scope
     throttle_scope = "low"
 
-    def get(self, request,pk):
+    def get(self, request, pk):
         # Get file ID and column name from the request data
-                
-        file_instance = File.objects.get(pk=pk) 
+
+        file_instance = File.objects.get(pk=pk)
         df = pd.read_excel(file_instance.file)
-        column_names = df.columns.to_list() 
-        file_name = os.path.basename(file_instance.file.name)            
+        column_names = df.columns.to_list()
+        file_name = os.path.basename(file_instance.file.name)
 
-
-        return Response({"file_id":f'{file_instance.id}',"file name": f'{file_name}',"available_columns": column_names},status=status.HTTP_201_CREATED)
-
-      
+        return Response({"file_id": f'{file_instance.id}', "file name": f'{file_name}', "available_columns": column_names}, status=status.HTTP_201_CREATED)
